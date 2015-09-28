ef27f10f286018bd5fbcd101cf5020ee2f34ae06
Niall Ryan <niallryanpersonal@gmail.com>
2015-09-28 19:02:38 +0100

    Test

---
+++ PracticeWPF/CodeAndUncompiledXAML.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Markup;
+
+namespace PracticeWPF
+{
+    /// <summary>
+    /// Interaction logic for CodeAndUncompiledXAML.xaml
+    /// </summary>
+    public partial class CodeAndUncompiledXAML : Window
+    {
+        private Button button1;
+
+        public CodeAndUncompiledXAML()
+        {
+            InitializeComponent();
+        }
+
+        public CodeAndUncompiledXAML(string xamlFile)
+        {
+            this.Width = this.Height = 285;
+            this.Left = this.Top = 100;
+            this.Title = "Dynamically Loaded XAML";
+
+            DependencyObject rootElement;
+            using(FileStream fs = new FileStream(xamlFile, FileMode.Open))
+            {
+                rootElement = (DependencyObject)XamlReader.Load(fs);
+            }
+
+            //insert the new markup into this window
+            this.Content = rootElement;
+
+            //Find the control with the appropriate name
+            button1 = (Button)LogicalTreeHelper.FindLogicalNode(rootElement, "button1");
+            
+            //the two lines below do the same thing as the line above
+            FrameworkElement frameworkElement = (FrameworkElement)rootElement;
+            button1 = (Button)frameworkElement.FindName("button1");
+
+            button1.Click += button1_Click;
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            button1.Content = "Thank you";
+        }
+    }
+}
---
+++ PracticeWPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticeWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+            //automatically generated when you compile your application. Calls the LoadComponent method of the System.Windows.Application class. 
+            //The LoadComponent() method extracts the BAML (compiled XAML) from your assembly and uses it to build your user interface. As it
+            //parses the BAML, it creates each control object, sets it properties, and attaches any event handlers. 
+            AskEightBall show = new AskEightBall();
+            show.Show();
+        }
+    }
+}
